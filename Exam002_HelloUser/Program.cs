21f1a49270ffc1486ab5537fbf425a8b0f372265
Rustam <imankulov.ru@gmail.com>
2022-09-01 13:21:30 +0300

    git ignore

---
+++ Exam002_HelloUser/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введи свое имя олень ");
+string username = Console.ReadLine();
+Console.WriteLine("Привет "); 
+Console.WriteLine(username);
+
+
+
+Console.WriteLine("Hello, World!");
